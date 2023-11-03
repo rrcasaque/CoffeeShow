671742ea995a3aaf81e4025f80a7794d8dbb3ba0
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-03 18:35:16 -0300

    feat: created api structure

---
+++ src/pages/api/update.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'PUT':
+      res.status(200).json({ message: 'entrou aqui' });
+      break;
+    default:
+      res.status(501).json({ error: 'método não implementado' });
+      break;
+  }
+}
