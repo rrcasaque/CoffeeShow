0a6c660135b9c9ff219bd3acce99698418331451
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-04 17:24:08 -0300

    feat: implemented open modals

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,14 @@
 import { DeleteIcon, EditIcon, PlusSquareIcon } from '@chakra-ui/icons';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useDisclosure } from '@chakra-ui/react';
 import Image from 'next/image';
+import { useState } from 'react';
+import { TableItemModal } from '../Modals/TableItemModal';
+import { CustomModal } from '../Modals/CustomModal';
+import { useItemStore } from '@/context/ItemStore';
 
 export const Sidebar = () => {
+  const [modal, setModal] = useState<JSX.Element>(<></>);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Flex
       minH="100vh"
@@ -30,6 +36,10 @@ export const Sidebar = () => {
           py="4px"
           borderRadius="14px"
           align="center"
+          id="create"
+          onClick={(e) => {
+            changeModal(e.currentTarget.id, setModal, onOpen);
+          }}
         >
           <PlusSquareIcon />
           <Text ml="12px">Adicionar Item</Text>
@@ -42,6 +52,10 @@ export const Sidebar = () => {
           py="4px"
           borderRadius="14px"
           align="center"
+          id="update"
+          onClick={(e) => {
+            changeModal(e.currentTarget.id, setModal, onOpen);
+          }}
         >
           <EditIcon />
           <Text ml="12px">Editar Item</Text>
@@ -54,11 +68,47 @@ export const Sidebar = () => {
           py="4px"
           borderRadius="14px"
           align="center"
+          id="delete"
+          onClick={(e) => {
+            changeModal(e.currentTarget.id, setModal, onOpen);
+          }}
         >
           <DeleteIcon />
           <Text ml="12px">Remover Item</Text>
         </Flex>
       </Flex>
+      <CustomModal isOpen={isOpen} onClose={onClose}>
+        {modal}
+      </CustomModal>
     </Flex>
   );
+};
+
+const changeModal = (
+  id: string,
+  setModal: React.Dispatch<React.SetStateAction<JSX.Element>>,
+  onOpen: () => void
+) => {
+  onOpen();
+  switch (id) {
+    case 'create':
+      setModal(<TableItemModal items={[]} modalType={'delete'} />);
+      break;
+    case 'update':
+      setModal(
+        <TableItemModal
+          items={useItemStore.getState().items}
+          modalType={'update'}
+        />
+      );
+      break;
+    case 'delete':
+      setModal(
+        <TableItemModal
+          items={useItemStore.getState().items}
+          modalType={'delete'}
+        />
+      );
+      break;
+  }
 };
