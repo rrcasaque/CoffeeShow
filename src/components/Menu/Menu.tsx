39951b497b90a147477c4a6a2724719e9979adb4
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-05 21:18:48 -0300

    feat: align itemCards in center

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,43 +1,29 @@
 import { useItemStore } from '@/context/ItemStore';
 import { Flex } from '@chakra-ui/react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { ItemCard } from '../ItemCard/ItemCard';
-import { Item } from '@/models/Item';
 
 export const Menu = () => {
-  const [itemsList, setItemsList] = useState<Item[]>();
-
-  useEffect(() => {
-    (async () => {
-      const itemList = await axios.get('/api/read');
-      setItemsList(itemList.data);
-      useItemStore.setState({ items: itemList.data });
-    })();
-  }, []);
-
+  const { items } = useItemStore();
   return (
     <Flex
       minH="100vh"
       w="full"
       ml="320px"
       wrap="wrap"
-      justify="space-around"
-      // bg="#3a2215"
+      justify="center"
       backgroundImage="https://www.creativefabrica.com/wp-content/uploads/2023/10/01/Rustic-Wood-Background-Seamless-Pattern-Graphics-80558264-3-580x435.png"
     >
-      {itemsList &&
-        itemsList.map((item) => {
-          return (
-            <ItemCard
-              key={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
-        })}
+      {items.map((item) => {
+        return (
+          <ItemCard
+            key={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        );
+      })}
     </Flex>
   );
 };
