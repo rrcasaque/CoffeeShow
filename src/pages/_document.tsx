3a26390d6b5f501ae313571f68fb2600fd641728
GattiGabriel <ggattidev@gmail.com>
2023-11-06 01:30:42 -0300

    feat: added correct name and icon to page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Coffe Show</title>
+        <link rel='shortcut icon' href='/coffe.png'></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
