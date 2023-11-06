731681701e4a2a0737820cf219668b6502b50739
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-06 19:38:55 -0300

    fix: changed server side request by client side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
 import { useItemStore } from '@/context/ItemStore';
 import { MainPage } from '@/modules/MainPage';
 import { ChakraProvider } from '@chakra-ui/react';
-import axios from 'axios';
-import { InferGetServerSidePropsType } from 'next';
 
-export const getServerSideProps = async () => {
-  const listItems = await axios.get('https://coffee-show.vercel.app/api/read');
-  return {
-    props: {
-      listItems: listItems.data,
-    },
-  };
-};
+// export const getServerSideProps = async () => {
+//   const listItems = await axios.get('https://coffee-show.vercel.app/api/read');
+//   return {
+//     props: {
+//       listItems: listItems.data,
+//     },
+//   };
+// };
 
 export default function Home(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
+  // props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
-  useItemStore.setState({ items: props.listItems });
+  // useItemStore.setState({ items: props.listItems });
   return (
     <ChakraProvider>
       <MainPage />
