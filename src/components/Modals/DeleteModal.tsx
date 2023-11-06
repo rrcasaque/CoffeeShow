2caf81d3f2c934b854870275e7f1ac2ea1e6dc9c
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-05 21:21:47 -0300

    feat: created deletemodal component

---
+++ src/components/Modals/DeleteModal.tsx
@@ -0,0 +1,49 @@
+import { useItemStore } from '@/context/ItemStore';
+import {
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Text,
+  Button,
+  Flex,
+} from '@chakra-ui/react';
+import axios from 'axios';
+
+interface DeleteModalProps {
+  id: string;
+  onClose: () => void;
+}
+
+export const DeleteModal = (props: DeleteModalProps) => {
+  return (
+    <ModalContent minW="380px">
+      <ModalHeader>Confirmação de exclusão</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody w="full" flexDirection="column">
+        <Flex direction="column">
+          <Text>Tem certeza que deseja excluir o item?</Text>
+          <Button
+            color="#ffffff"
+            bg="red.400"
+            w="80px"
+            alignSelf="flex-end"
+            mt="12px"
+            onClick={async () => {
+              await axios.delete('/api/delete', {
+                data: {
+                  itemID: props.id,
+                },
+              });
+              const updatedList = await axios.get('/api/read');
+              useItemStore.setState({ items: updatedList.data });
+              props.onClose();
+            }}
+          >
+            Excluir
+          </Button>
+        </Flex>
+      </ModalBody>
+    </ModalContent>
+  );
+};
