44c91ccca8846e9250665c45baca45e63a8ee604
Mateus Vieira <mateus.sly.mp@gmail.com>
2023-11-29 10:53:26 -0300

    fix: change access modifiers of EditTest and RemoveTest

--- CoffeShowTest/src/test/java/EditTest.java
+++ CoffeShowTest/src/test/java/EditTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class EditTest {
+ class EditTest {
     WebDriver driver;
     WebDriverWait wait;
 
